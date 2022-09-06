c374b95fce138e45da0442a8e09c6c1150da884b
Hassan Alkhalidi <5357529+Gruce@users.noreply.github.com>
2022-09-06 17:57:48 +0300

    Update Conversations.vue

--- src/runtime/components/Support/Conversations.vue
+++ src/runtime/components/Support/Conversations.vue
@@ -1,5 +1,13 @@
 <template>
-    <div>
-        oqwijd
+    <div flex="~" border="b secondary dark:secondaryOp" pb="1" mb="1">
+        <div p="y-1 x-1.5" rounded="~" cursor="pointer" transition="~ duration-200 ease-in-out" text="xs secondaryOp dark:secondary hover:primary dark:hover:primaryOp" bg="secondary dark:secondaryOp hover:primaryOp dark:hover:primary">
+            اسم الشخص
+        </div>
     </div>
 </template>
+
+<script setup>
+// import { useSupabaseClient, useUser } from '#imports'
+// const supabase = useSupabaseClient()
+// const user = useUser()
+</script>
