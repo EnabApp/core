6f782ebe148d03df78ffd305cd01fcfc9a237653
Hussein Najah <husseinnajah123@gmail.com>
2022-09-07 19:07:31 +0300

    Update Message.vue

--- src/runtime/components/Support/Message.vue
+++ src/runtime/components/Support/Message.vue
@@ -1,11 +1,11 @@
 <template>
     <div w="70%" flex="~ col gap-1" :class="{ 'self-end': user.id !== message.user_id }">
-        <span v-if="!same" text="xs secondary dark:secondaryOp">{{message.profiles.username}}</span>
+        <span v-if="!same" text="xs primaryOp dark:primary">{{message.profiles.username}}</span>
         <div :class="[
             user.id == message.user_id
             ? 'bg-opacity-30 dark:bg-opacity-30'
             : 'bg-opacity-100 dark:bg-opacity-100'
-        ]" bg="secondary dark:secondaryOp" class="break-words" text="sm" p="2" rounded="~">
+        ]" bg="secondary dark:secondaryOp" class="break-words" text="sm  primaryOp dark:primary" p="2" rounded="~">
             {{message.message}}
         </div>
     </div>
