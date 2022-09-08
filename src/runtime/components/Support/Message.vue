0b9b6f25ea4569fbdfc31cabad613405219f4abc
Hassan Alkhalidi <5357529+Gruce@users.noreply.github.com>
2022-09-08 20:06:39 +0300

    Update Message.vue

--- src/runtime/components/Support/Message.vue
+++ src/runtime/components/Support/Message.vue
@@ -1,27 +1,14 @@
 <template>
-  <div
-    w="70%"
-    flex="~ col gap-2px"
-    :class="{ 'self-end text-left': user.id !== messageUser?.id }"
-  >
+  <div w="70%" flex="~ col gap-2px" :class="{ 'self-end text-left': user.id !== messageUser?.id }">
     <!-- User name can be either primary or secondaryOp -->
     <span v-if="user.id !== messageUser?.id" text="xs secondaryOp">{{
-      messageUser?.username
+    messageUser?.username
     }}</span>
-    <div
-      w="fit"
-      min-w="50%"
-      :class="[
-        user.id == messageUser?.id
-          ? 'bg-secondary dark:text-primaryOp dark:text-primary'
-          : 'bg-secondaryOp text-secondary self-end',
-      ]"
-      class="break-words"
-      text="16px right"
-      font="medium"
-      p="x-5 y-2"
-      rounded="5px"
-    >
+    <div w="fit" min-w="50%" :class="[
+      user.id == messageUser?.id
+        ? 'bg-secondary dark:text-primaryOp dark:text-primary'
+        : 'bg-secondaryOp text-secondary self-end',
+    ]" class="break-words" text="16px right" font="medium" p="x-5 y-2" rounded="5px">
       {{ message.message }}
     </div>
   </div>
