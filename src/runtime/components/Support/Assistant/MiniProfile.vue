b80784d50e22da4de4d2238fb1ccceb2b3bdf6ee
Ahmed <mr.darkboss1@gmail.com>
2022-09-10 01:00:43 +0300

    d

--- src/runtime/components/Support/Assistant/MiniProfile.vue
+++ src/runtime/components/Support/Assistant/MiniProfile.vue
@@ -1,14 +1,14 @@
 <template>
   <div
-    flex="~ gap-3"
-    p="y-2 x-4"
+    flex="~"
+    p="2"
     bg="secondaryOp hover:opacity-70"
     rounded="5px"
     items="center"
     justify="between"
     cursor="pointer"
   >
-  <div flex="~ gap-1" items="center">
+  <div flex="~ gap-3" items="center">
     <div w="30px" v-if="!(md || sm)">
     <IconBack
         @click="$emit('unselect')"
@@ -18,7 +18,7 @@
         cursor="pointer"
       />
   </div>
-    <div flex="~ gap-3" >
+    <div flex="~ gap-3" max-w="200px">
       <div relative="~">
         <img
           src="https://d1oqwsnd25kjn6.cloudfront.net/production/curio_primary_images/12566/original/Feb._25_2018-SteveJobs.jpg?1518809726"
@@ -36,7 +36,7 @@
           class="bg-secondaryOp dark:bg-secondary"
         ></div>
       </div>
-      <div flex="~ col gap-1" w="60%">
+      <div flex="~ col gap-1" max-w="80%">
         <span text="primaryOp dark:primary" truncate="~" font="semibold"
           >حسن الخالدي حسابي الثاني</span
         >
@@ -46,7 +46,7 @@
   </div>
 
 
-    <div flex="~ gap-2" justify="between" w="30px" px="1">
+    <div flex="~ gap-2" justify="between" w="30px" mx="2" px="1">
       <IconLock v-if="showLock" @click="showLock=false" w="24px" h="24px" text="primary" cursor="pointer"/>
       <IconUnlock v-else @click="showLock=true" w="24px" h="24px" text="error" cursor="pointer"/>
 
