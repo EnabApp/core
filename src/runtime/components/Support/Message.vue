40227a6c92708735f52db041a805ac9d677c10bc
Hassan Alkhalidi <5357529+Gruce@users.noreply.github.com>
2022-09-10 23:32:07 +0300

    emojis update

--- src/runtime/components/Support/Message.vue
+++ src/runtime/components/Support/Message.vue
@@ -8,7 +8,11 @@
       user.id == messageUser?.id
         ? 'bg-secondary dark:text-primaryOp dark:text-primary self-start'
         : 'bg-secondaryOp text-secondary self-end',
-    ]" class="break-words" text="16px right" font="medium" p="x-3 y-2" rounded="10px">
+
+      styleEmoji
+        ? 'text-[32px]'
+        : 'text-[16px]'
+    ]" class="break-words" text="right" font="medium" p="x-3 y-2" rounded="10px">
       {{ message.message }}
     </div>
   </div>
@@ -19,4 +23,21 @@ import { useUser } from "../../composables/states";
 const user = useUser();
 const props = defineProps(["message", "same"]);
 const messageUser = computed(() => props.message.sender_id);
+
+
+
+const checkingEmojis = (text) => {
+  const onlyEmojis = text.replace(new RegExp('[\u0000-\u1eeff]', 'g'), '')
+  const visibleChars = text.replace(new RegExp('[\n\r\s]+|( )+', 'g'), '')
+  const hasOnlyEmojis = onlyEmojis.length === visibleChars.length
+  if (hasOnlyEmojis) {
+    const countEmojies = Array.from(text.split(/[\ufe00-\ufe0f]/).join("")).length;
+    return countEmojies <= 1
+  }
+  return false
+
+}
+
+const styleEmoji = checkingEmojis(props.message.message)
+
 </script>
