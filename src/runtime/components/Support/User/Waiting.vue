29265fb89ce515935a61affad4bc391df8b4e515
Hussein Najah <husseinnajah123@gmail.com>
2022-09-11 19:07:22 +0300

    Update Waiting.vue

--- src/runtime/components/Support/User/Waiting.vue
+++ src/runtime/components/Support/User/Waiting.vue
@@ -3,7 +3,7 @@
     <div flex="~ col" h="full" w="full" justify="center" items="center">
       <IconLoading w="20" h="20" text="primaryOp dark:primary" />
       <span text="primaryOp dark:primary" font="semibold" class="mt-2"
-        >سيتم الرد عليك من الدعم الفني قريبا</span
+        >سيتـم الـرد علـيك من قبل الـدعـم الـفني قريـبـا</span
       >
     </div>
   </div>
