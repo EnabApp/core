06f942d49a06adb26be9068dd71ef4b705a4a0a7
Hasan Alkarnosy <gruceing@gmail.com>
2022-09-19 11:42:30 +0300

    release-v1.8.22-beta.19

--- src/runtime/components/LoadingDesktop.vue
+++ src/runtime/components/LoadingDesktop.vue
@@ -14,6 +14,8 @@
   </div>
 </template>
 <script setup>
+import { onMounted } from '#imports'
+
 onMounted(() => {
   const cursor = document.querySelector("#cursor");
   const cursorBorder = document.querySelector("#cursor-border");
