dd834b655dc388f8d05c19780cebd6c598bac192
Manar Naseer <79095180+6vnar@users.noreply.github.com>
2022-09-27 21:55:39 +0300

    add

--- playground/pages/test.vue
+++ playground/pages/test.vue
@@ -1,28 +1,17 @@
 <template>
-<!--
-  <button @click="support.fetchUnSolvedConversations()">المحادثات الغير محلوله </button>
-  <button @click="support.fetchSolvedConversations()">المحادثات المحلوله </button>
-  <button @click="support.fetchProfile('ffd321ff-c21b-4d52-aa89-4bef48308369')">بروفايل </button>
-  <button @click="support.closeConversation('62500f2e-b625-44ba-bf80-4e4133055cf1')">اغلاق المحادثة </button>
-  <button @click="support.setAssistant('f14c4d98-3104-431e-bc53-baa272e40e37')">اختيار المساعد </button>
-  <button @click="support.fetchMessages('a30245bb-e2ba-4102-9691-0629ae9671e4')">الرسايل </button> -->
 
-  <button @click="support.fetchUnSolvedConversations()">المحادثات الغير محلوله </button>
-  <button @click="support.startConversation('hi', 1)"> بدء المحادثة </button>
 
   <div>
-    {{support.getUnSolvedConversations}}
+    {{ appManager.getApps }}
   </div>
 
 </template>
 
 <script setup>
-import { useSupport } from '#imports'
-const support = useSupport()
-//console.log(support.getUnSolvedConversations)
-// console.log(support.getSolvedConversations)
-// console.log(support.getProfile)
-// console.log(support.getMessages)
+import { useAppManager } from '#imports'
+const appManager= useAppManager()
+appManager.fetchLastSixApps()
+console.log(appManager.getApps)
 
 </script>
 
--- src/runtime/composables/useAppManager.ts
+++ src/runtime/composables/useAppManager.ts
@@ -72,87 +72,108 @@ export const useAppManager = defineStore("app-manager", {
       this.apps = apps.map((app) => new App(app))
       this.apps.push(...this.developmentApps)
     },
-    async fetchPacks() {
+
+    async fetchLastSixApps() {
       const supabase = useSupabaseClient();
-      let { data: packs, error } = await supabase
-        .from("packs")
-        .select("*,packs_apps(app:app_id(*,apps_services(*,users_services(*)),apps_plans(*,users_plans(*))))")
-        if (error) return error
-      this.packs = packs.map((pack) => new Pack(pack));
-    },
-    // Set Focus to an App
-    async setFocus(id) {
-      this.appLayers = this.appLayers.filter((app_id) => app_id !== id);
-      this.appLayers.push(id);
-      this.focused = id;
-    },
-
-    // Adding app for development purposes
-    addApp(app) {
-      if (!process.dev) return;
-      app.title = `[ ${app.title} ]`;
-      let newApp = new App(app);
-      newApp.owned = true;
-      newApp.id = this.developmentApps.length + 1 + 1000000;
-      this.developmentApps.push(newApp);
-    },
-
-    // Buy an app
-    async buyApp(id: number) {
-      const supabase = useSupabaseClient()
-      let { data, error } = await supabase.functions.invoke('core-buy-app', {
-        body: JSON.stringify({ app_id: id, buy_type: 1 }),
-      })
-      this.fetch();
-      if (error) return error;
-      return data;
-
-    },
-
-    //Buy a plan
-    async buyPlan(id: number) {
-      const supabase = useSupabaseClient()
-      let { data, error } = await supabase.functions.invoke('core-buy-app', {
-        body: JSON.stringify({ plan_id: id, buy_type: 2 }),
-      })
-      this.fetch();
-      if (error) return error;
-      return data;
-
-    },
-
-    // Buy a service
-    async buyService(id: number) {
-      // const { $toast } = useNuxtApp()
-      const supabase = useSupabaseClient();
-      const user = useUser();
-      let { data, error } = await supabase.rpc("buyService", {
-        _service_id: id,
-        _user_id: user.value.id,
-      });
-      if (error) {
-        console.log("حدث خطأ اثناء شراء الخدمة");
-        return false;
-      }
-      if (!data) {
-        console.log("لاتمتلك مايكفي من النقاط");
-        return false;
-      }
-      this.fetch();
-    },
-
-    // Buy an package
-    async buyPack(pack_id) {
-      const supabase = useSupabaseClient();
-
-      let { data, error } = await supabase.functions.invoke("core-buy-pack", {
-        body: JSON.stringify({ pack_id: pack_id }),
-      });
-      this.fetch();
-      if (error) return error;
-      else return data;
-    },
+      let { data: apps, error } = await supabase.from("apps")
+        .select(`
+                  *,
+                  users_apps(id),
+                  apps_services(
+                      id, app_id, title, points, icon, description, users_services(id)
+                  ),
+                  apps_plans(
+                      id, app_id, type, points, description, users_plans(id)
+                  )
+              `)
+        .order("id", { ascending: false })
+        .limit(6)
+      if(error) return error
+      this.apps = apps.map((app) => new App(app))
+      this.apps.push(...this.developmentApps)
   },
+
+  async fetchPacks() {
+    const supabase = useSupabaseClient();
+    let { data: packs, error } = await supabase
+      .from("packs")
+      .select("*,packs_apps(app:app_id(*,apps_services(*,users_services(*)),apps_plans(*,users_plans(*))))")
+    if (error) return error
+    this.packs = packs.map((pack) => new Pack(pack));
+  },
+  // Set Focus to an App
+  async setFocus(id) {
+    this.appLayers = this.appLayers.filter((app_id) => app_id !== id);
+    this.appLayers.push(id);
+    this.focused = id;
+  },
+
+  // Adding app for development purposes
+  addApp(app) {
+    if (!process.dev) return;
+    app.title = `[ ${app.title} ]`;
+    let newApp = new App(app);
+    newApp.owned = true;
+    newApp.id = this.developmentApps.length + 1 + 1000000;
+    this.developmentApps.push(newApp);
+  },
+
+  // Buy an app
+  async buyApp(id: number) {
+    const supabase = useSupabaseClient()
+    let { data, error } = await supabase.functions.invoke('core-buy-app', {
+      body: JSON.stringify({ app_id: id, buy_type: 1 }),
+    })
+    this.fetch();
+    if (error) return error;
+    return data;
+
+  },
+
+  //Buy a plan
+  async buyPlan(id: number) {
+    const supabase = useSupabaseClient()
+    let { data, error } = await supabase.functions.invoke('core-buy-app', {
+      body: JSON.stringify({ plan_id: id, buy_type: 2 }),
+    })
+    this.fetch();
+    if (error) return error;
+    return data;
+
+  },
+
+  // Buy a service
+  async buyService(id: number) {
+    // const { $toast } = useNuxtApp()
+    const supabase = useSupabaseClient();
+    const user = useUser();
+    let { data, error } = await supabase.rpc("buyService", {
+      _service_id: id,
+      _user_id: user.value.id,
+    });
+    if (error) {
+      console.log("حدث خطأ اثناء شراء الخدمة");
+      return false;
+    }
+    if (!data) {
+      console.log("لاتمتلك مايكفي من النقاط");
+      return false;
+    }
+    this.fetch();
+  },
+
+  // Buy an package
+  async buyPack(pack_id) {
+    const supabase = useSupabaseClient();
+
+    let { data, error } = await supabase.functions.invoke("core-buy-pack", {
+      body: JSON.stringify({ pack_id: pack_id }),
+    });
+    this.fetch();
+    if (error) return error;
+    else return data;
+  },
+},
 });
 
 if (import.meta.hot) {
